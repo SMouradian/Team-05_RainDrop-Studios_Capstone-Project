f86f772bb8e44511efff960435d17be4415ddd04
Simeon Dimitrov <simeon.dimitrov164@gmail.com>
2025-11-19 00:57:43 -0800

    Add enemy basic attack

--- Raindrop Studio Capstone/Assets/Scripts/Player/PlayerAttributes/PlayerHealth.cs
+++ Raindrop Studio Capstone/Assets/Scripts/Player/PlayerAttributes/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
+    private bool isDead = false;
 
     public void Start()
     {
@@ -12,8 +14,17 @@ public class PlayerHealth : MonoBehaviour
     }
     public void TakeDamage(int damageAmount)
     {
+        if (isDead) return;
+
         currentHealth -= damageAmount;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        Debug.Log("Player took " + damageAmount + " damage. Current health: " + currentHealth);
+        if (currentHealth <= 0)
+        {
+            isDead = true;
+            Debug.Log("Player is dead.");
+        }
     }
 
     public void Heal(int healAmount)

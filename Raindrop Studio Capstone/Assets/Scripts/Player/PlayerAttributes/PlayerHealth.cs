2acdeec4b6d7b9c48d23cccff297730c70ecda44
Simeon Dimitrov <simeon.dimitrov164@gmail.com>
2025-11-19 16:33:33 -0800

    Add screen shake when taking damage

--- Raindrop Studio Capstone/Assets/Scripts/Player/PlayerAttributes/PlayerHealth.cs
+++ Raindrop Studio Capstone/Assets/Scripts/Player/PlayerAttributes/PlayerHealth.cs
@@ -8,9 +8,19 @@ public class PlayerHealth : MonoBehaviour
     private int currentHealth;
     private bool isDead = false;
 
+    [Header("Camera Shake")]
+    [SerializeField] private CameraShake cameraShake;
+    [SerializeField] private float shakeDuration = 0.15f;
+    [SerializeField] private float shakeMagnitude = 0.15f;
+
     public void Start()
     {
         currentHealth = maxHealth;
+
+        if (cameraShake == null)
+        {
+            cameraShake = FindObjectOfType<CameraShake>();
+        }
     }
     public void TakeDamage(int damageAmount)
     {
@@ -20,6 +30,12 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
         Debug.Log("Player took " + damageAmount + " damage. Current health: " + currentHealth);
+        
+        if (cameraShake != null)
+        {
+            cameraShake.ShakeOnce(shakeDuration, shakeMagnitude);
+        }
+        
         if (currentHealth <= 0)
         {
             isDead = true;
